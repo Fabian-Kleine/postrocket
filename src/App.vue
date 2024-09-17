399d8878bd33696080668edac8f32fcbf8405b0d
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-17 11:01:47 +0200

    change App.vue
    remove default setup generated by vite

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div class="flex justify-center w-screen gap-40">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="w-48" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="w-48" alt="Vue logo" />
-    </a>
+  <div class="flex justify-center items-center w-screen gap-4">
+    <img src="/icon.png" class="w-48" alt="Logo" />
+    <h1 class="text-6xl text-primary font-bold">Postman Clone</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
