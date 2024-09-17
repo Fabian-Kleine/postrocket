eeab5e415cb3a325baf1d7718668effca32c8005
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-17 16:02:59 +0200

    remove placeholder content

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,4 @@
 <template>
   <Navbar />
   <Tabs />
-  <div class="flex justify-center items-center w-screen gap-4">
-    <img src="/icon.png" class="w-48" alt="Logo" />
-    <h1 class="text-6xl text-primary font-bold">Postman Clone</h1>
-  </div>
 </template>
