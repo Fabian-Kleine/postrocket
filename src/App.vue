a23671c5191dbc754c33720738bfbcb07c5527a9
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-17 14:45:45 +0200

    add Tabs
    add Tabs for different requests

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
   import Navbar from "./components/Navbar.vue";
+  import Tabs from "./components/Tabs.vue";
 </script>
 
 <template>
   <Navbar />
+  <Tabs />
   <div class="flex justify-center items-center w-screen gap-4">
     <img src="/icon.png" class="w-48" alt="Logo" />
     <h1 class="text-6xl text-primary font-bold">Postman Clone</h1>
