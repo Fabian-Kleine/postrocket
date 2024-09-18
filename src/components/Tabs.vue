5d486a3719bd3d74fd9ef9163811b69fafc00ab1
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-09-18 08:45:52 +0200

    refactor: move input field into its own component
    move input field into MainInput.vue component and add close on click outside to method select

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -2,6 +2,7 @@
 import { XMarkIcon } from '@heroicons/vue/24/outline'
 import { Ref, ref } from 'vue';
 import Button from './Button.vue';
+import MainInput from './MainInput.vue';
 
 const tabs = ref([
     {
@@ -20,8 +21,6 @@ const tabs = ref([
 
 const activeTab: Ref<number, number> = ref(0);
 
-const selectOpen: Ref<boolean, boolean> = ref(false);
-
 function closeTab(i: number) {
     tabs.value.splice(i, 1);
     tabs.value = [...tabs.value];
@@ -34,13 +33,6 @@ function closeTab(i: number) {
 
 function openTab(i: number) {
     activeTab.value = i;
-}
-
-function handleMethodInput(e: Event) {
-    const target = (<HTMLInputElement>e.target);
-
-    tabs[activeTab.value].method = target.value;
-    selectOpen.value = false;
 }
 </script>
 
@@ -68,15 +60,7 @@ function handleMethodInput(e: Event) {
     </div>
     <div class="max-w-4xl mt-12">
         <div class="flex gap-2 mx-4 w-full">
-            <div class="relative flex border border-default-200 dark:border-default-700 p-2 rounded-md flex-grow">
-                <input @focus="selectOpen = !selectOpen" @input="handleMethodInput" :class="['bg-background dark:bg-foreground font-bold w-24 outline-offset-4 outline-4 focus:outline-primary', tabs[activeTab].method == 'POST' ? 'text-warning' : '' , tabs[activeTab].method == 'GET' ? 'text-success' : '']" :value="tabs[activeTab].method" />
-                <div v-if="selectOpen" ref="methodsSelect" class="absolute top-12 left-0 min-w-32 p-2 bg-default-100 dark:bg-default-800 rounded-md shadow-lg">
-                    <div @click="tabs[activeTab].method = 'POST'; selectOpen = false;" class="text-warning font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md">POST</div>
-                    <div @click="tabs[activeTab].method = 'GET'; selectOpen = false;" class="text-success font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md">GET</div>
-                </div>
-                <div class="w-[1px] mx-4 h-full bg-default-200 dark:bg-default-700"></div>
-                <input type="text" v-model="tabs[activeTab].url" class="bg-background dark:bg-foreground flex-grow w-full h-full outline-offset-8 outline-4 focus:outline-primary" />
-            </div>
+            <MainInput :tabs="tabs" :active-tab="activeTab" />
             <Button children="Senden" />
         </div>
     </div>
