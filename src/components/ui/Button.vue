975480b02064a86800b0cd8b7a8e40b8d9d4fa25
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-09-18 09:47:01 +0200

    add button variants
    add all color variants as prop to button component

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,8 +1,21 @@
-<script setup>
+<script setup lang="ts">
+interface Props {
+    variant: "primary" | "secondary" | "default" | "warning" | "success" | "danger";
+}
 
-    const props = defineProps(['children']);
+const { variant } = defineProps<Props>();
+const className = [
+    "text-background transition-colors font-bold rounded px-3 text-sm",
+    variant == "primary" ? "bg-primary hover:bg-primary-600"
+        : variant == "secondary" ? "bg-secondary hover:bg-secondary-600"
+            : variant == "default" ? "bg-default-600 hover:bg-default-700"
+                : variant == "danger" ? "bg-danger hover:bg-danger-600"
+                    : variant == "success" ? "bg-success hover:bg-success-600"
+                        : variant == "warning" ? "bg-warning hover:bg-warning-600" : ""];
 </script>
 
 <template>
-    <button type="button" class="bg-primary text-background transition-colors font-bold rounded px-3 text-sm hover:bg-primary-600">{{ children }}</button>
+    <button type="button" :class="className">
+        <slot></slot>
+    </button>
 </template>
