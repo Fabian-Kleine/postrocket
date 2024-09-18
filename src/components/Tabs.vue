d975ba238b86bca8716cad334dd71659e3d8a7a7
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-18 10:51:06 +0200

    add Radio select for body format
    add radio selection for body format of the request

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -2,6 +2,8 @@
 import { XMarkIcon, PlusIcon } from '@heroicons/vue/24/outline'
 import { onMounted, Ref, ref, watch } from 'vue';
 import Button from './ui/Button.vue';
+import Radio from './ui/Radio.vue';
+import Label from './ui/Label.vue';
 import MainInput from './MainInput.vue';
 
 type Tab = {
@@ -35,10 +37,10 @@ watch(tabs, () => {
 
 watch(activeTab, () => {
     localStorage.setItem('activeTab', JSON.stringify(activeTab.value));
-}, {deep: true});
+}, { deep: true });
 
 onMounted(() => {
-    if ("tabs" in localStorage){
+    if ("tabs" in localStorage) {
         const savedTabs = localStorage.getItem('tabs');
         const parsedSavedTabs = JSON.parse(savedTabs || "");
 
@@ -61,8 +63,8 @@ onMounted(() => {
 <template>
     <div class="flex justify-left items-center border-b border-default-200 dark:border-default-700">
         <div class="flex justify-center items-center relative -bottom-[1px] group z-10"
-            :class="activeTab == index ? 'bg-background dark:bg-foreground' : ''"
-            v-for="(tab, index) in tabs" :title="tab.url || 'Unbenannter Request'" v-if="tabs.length > 0">
+            :class="activeTab == index ? 'bg-background dark:bg-foreground' : ''" v-for="(tab, index) in tabs"
+            :title="tab.url || 'Unbenannter Request'" v-if="tabs.length > 0">
             <div @click="openTab(index)"
                 class="flex justify-left items-center gap-2 w-56 px-3 py-4 text-sm cursor-pointer border-default-200 dark:border-default-700"
                 :class="activeTab == index ? 'border-l border-r' : ''">
@@ -79,18 +81,42 @@ onMounted(() => {
                 <XMarkIcon class="text-foreground dark:text-default size-5" />
             </button>
         </div>
-        <div @click="tabs.push({ method: 'GET', url: '' }); activeTab = tabs.length-1;" class="flex justify-left items-center gap-2 ml-2 h-fit p-2 rounded-md text-sm cursor-pointer transition-colors border-default-200 dark:border-default-700 hover:bg-default-300/50 dark:hover:bg-default-600">
+        <div @click="tabs.push({ method: 'GET', url: '' }); activeTab = tabs.length - 1;"
+            class="flex justify-left items-center gap-2 ml-2 h-fit p-2 rounded-md text-sm cursor-pointer transition-colors border-default-200 dark:border-default-700 hover:bg-default-300/50 dark:hover:bg-default-600">
             <PlusIcon class="text-foreground dark:text-default size-5" />
         </div>
     </div>
     <div class="max-w-4xl mt-12 mx-4" v-if="tabs.length > 0">
         <div class="flex justify-start items-center gap-2">
-            <div class="text-primary-400 uppercase font-bold p-1 text-sm border border-default-200 dark:border-default-700 rounded-md">http</div>
+            <div
+                class="text-primary-400 uppercase font-bold p-1 text-sm border border-default-200 dark:border-default-700 rounded-md">
+                http</div>
             <h2 class="my-4 cursor-default">{{ tabs[activeTab].url || "Unbenannter Request" }}</h2>
         </div>
         <div class="flex gap-2 w-full">
             <MainInput :tabs="tabs" :active-tab="activeTab" />
             <Button variant="primary">Senden</Button>
         </div>
+        <h2 class="my-4 font-bold text-lg">Body</h2>
+        <Label for="none-radio">
+            <Radio checked variant="primary" name="body-format" id="none-radio" value="none" />
+            none
+        </Label>
+        <Label for="form-data-radio">
+            <Radio variant="primary" name="body-format" id="form-data-radio" value="form-data" />
+            form-data
+        </Label>
+        <Label for="x-www-form-urlencoded-radio">
+            <Radio variant="primary" name="body-format" id="x-www-form-urlencoded-radio" value="x-www-form-urlencoded" />
+            x-www-form-urlencoded
+        </Label>
+        <Label for="JSON-radio">
+            <Radio variant="primary" name="body-format" id="JSON-radio" value="JSON" />
+            JSON
+        </Label>
+        <Label for="XML-radio">
+            <Radio variant="primary" name="body-format" id="XML-radio" value="XML" />
+            XML
+        </Label>
     </div>
 </template>
