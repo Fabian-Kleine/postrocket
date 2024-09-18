33710c0b9fed7481ed9a4f9d018a7058bb26abc0
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-09-18 10:47:34 +0200

    add Label and Radio component

---
+++ src/components/ui/Label.vue
@@ -0,0 +1,5 @@
+<template>
+    <label class="inline-flex justify-center items-center gap-2 mr-4 cursor-pointer">
+        <slot></slot>
+    </label>
+</template>
---
+++ src/components/ui/Radio.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+    variant: "primary" | "secondary" | "default" | "warning" | "success" | "danger";
+}
+
+const { variant } = defineProps<Props>();
+const className = [
+    "bg-default px-4 text-sm size-4",
+    variant == "primary" ? "accent-primary hover:accent-primary-600"
+        : variant == "secondary" ? "bg-secondary hover:bg-secondary-600"
+            : variant == "default" ? "bg-default-600 hover:bg-default-700"
+                : variant == "danger" ? "bg-danger hover:bg-danger-600"
+                    : variant == "success" ? "bg-success hover:bg-success-600"
+                        : variant == "warning" ? "bg-warning hover:bg-warning-600" : ""];
+</script>
+
+<template>
+    <input type="radio" :class="className">
+</template>
