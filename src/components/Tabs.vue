74886c741582c73ec8269f5bc3ecae6075339dc5
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-09-19 10:24:10 +0200

    add input fields
    add input fields for body
    - tables for form-data
    - textarea for JSON and XML

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { XMarkIcon, PlusIcon } from '@heroicons/vue/24/outline'
+import { XMarkIcon, PlusIcon, TrashIcon } from '@heroicons/vue/24/outline'
 import { onMounted, Ref, ref, watch } from 'vue';
 import Button from './ui/Button.vue';
 import Radio from './ui/Radio.vue';
@@ -11,11 +11,13 @@ const tabs: Tabs = ref([]);
 
 const activeTab: Ref<number, number> = ref(0);
 
-const activeBodyType: Ref<string, string> = ref("none");
+const activeBodyType: Ref<bodyTypeType, bodyTypeType> = ref("none");
 
 const setBodyType = (bodyType: bodyTypeType) => {
     activeBodyType.value = bodyType;
-    tabs.value[activeTab.value] = {...tabs.value[activeTab.value], body: { type: bodyType }};
+    const newTab = { ...tabs.value[activeTab.value], body: { content: tabs.value[activeTab.value].body.content, type: bodyType } };
+    tabs.value[activeTab.value] = newTab;
+    localStorage.setItem('tabs', JSON.stringify(newTab));
 }
 
 function closeTab(i: number) {
@@ -32,6 +34,59 @@ function openTab(i: number) {
     activeTab.value = i;
     activeBodyType.value = tabs.value[activeTab.value]?.body?.type || "none";
 }
+
+function addTab() {
+    tabs.value.push({
+        method: 'GET',
+        url: '',
+        body: {
+            type: 'none',
+            content: {
+                JSON: "{}",
+                XML: undefined,
+                formData: [{ active: null, key: '', value: '' }],
+                xWWWFormData: [{ active: null, key: '', value: '' }],
+            }
+        }
+    });
+    activeTab.value = tabs.value.length - 1;
+}
+
+function updateBodyContent(e: Event) {
+    const target = <HTMLTextAreaElement>e.target;
+
+    if (activeBodyType.value == "JSON") {
+        tabs.value[activeTab.value].body.content.JSON = target.value;
+    }
+    if (activeBodyType.value == "XML") {
+        tabs.value[activeTab.value].body.content.XML = target.value;
+    }
+}
+
+function addRowIfNeeded(index: number) {
+    if (activeBodyType.value == "x-www-form-urlencoded") {
+        if (index === tabs.value[activeTab.value].body.content.xWWWFormData.length - 1 && (tabs.value[activeTab.value].body.content.xWWWFormData[index].key != "" || tabs.value[activeTab.value].body.content.xWWWFormData[index].value != "")) {
+            tabs.value[activeTab.value].body.content.xWWWFormData[index].active = true;
+            tabs.value[activeTab.value].body.content.xWWWFormData.push({ active: null, key: '', value: '' });
+        }
+    }
+    if (activeBodyType.value == "form-data") {
+        if (index === tabs.value[activeTab.value].body.content.formData.length - 1 && (tabs.value[activeTab.value].body.content.formData[index].key != "" || tabs.value[activeTab.value].body.content.formData[index].value != "")) {
+            tabs.value[activeTab.value].body.content.formData[index].active = true;
+            tabs.value[activeTab.value].body.content.formData.push({ active: null, key: '', value: '' });
+        }
+    }
+}
+
+function rmRow(index: number) {
+    if (activeBodyType.value == "form-data") {
+        tabs.value[activeTab.value].body.content.formData.splice(index, 1);
+    }
+
+    if (activeBodyType.value == "x-www-form-urlencoded") {
+        tabs.value[activeTab.value].body.content.xWWWFormData.splice(index, 1);
+    }
+ }
 
 watch(tabs, () => {
     localStorage.setItem('tabs', JSON.stringify(tabs.value));
@@ -84,7 +139,7 @@ onMounted(() => {
                 <XMarkIcon class="text-foreground dark:text-default size-5" />
             </button>
         </div>
-        <div @click="tabs.push({ method: 'GET', url: '' }); activeTab = tabs.length - 1;"
+        <div @click="addTab"
             class="flex justify-left items-center gap-2 ml-2 h-fit p-2 rounded-md text-sm cursor-pointer transition-colors border-default-200 dark:border-default-700 hover:bg-default-300/50 dark:hover:bg-default-600">
             <PlusIcon class="text-foreground dark:text-default size-5" />
         </div>
@@ -102,24 +157,91 @@ onMounted(() => {
         </div>
         <h2 class="my-4 font-bold text-lg">Body</h2>
         <Label for="none-radio">
-            <Radio :checked="activeBodyType == 'none'" @input="setBodyType('none')" variant="primary" name="body-format" id="none-radio" />
+            <Radio :checked="activeBodyType == 'none'" @input="setBodyType('none')" variant="primary" name="body-format"
+                id="none-radio" />
             none
         </Label>
         <Label for="form-data-radio">
-            <Radio :checked="activeBodyType == 'form-data'" @input="setBodyType('form-data')" variant="primary" name="body-format" id="form-data-radio" />
+            <Radio :checked="activeBodyType == 'form-data'" @input="setBodyType('form-data')" variant="primary"
+                name="body-format" id="form-data-radio" />
             form-data
         </Label>
         <Label for="x-www-form-urlencoded-radio">
-            <Radio :checked="activeBodyType == 'x-www-form-urlencoded'" @input="setBodyType('x-www-form-urlencoded')" variant="primary" name="body-format" id="x-www-form-urlencoded-radio" />
+            <Radio :checked="activeBodyType == 'x-www-form-urlencoded'" @input="setBodyType('x-www-form-urlencoded')"
+                variant="primary" name="body-format" id="x-www-form-urlencoded-radio" />
             x-www-form-urlencoded
         </Label>
         <Label for="JSON-radio">
-            <Radio :checked="activeBodyType == 'JSON'" @input="setBodyType('JSON')" variant="primary" name="body-format" id="JSON-radio" />
+            <Radio :checked="activeBodyType == 'JSON'" @input="setBodyType('JSON')" variant="primary" name="body-format"
+                id="JSON-radio" />
             JSON
         </Label>
         <Label for="XML-radio">
-            <Radio :checked="activeBodyType == 'XML'" @input="setBodyType('XML')" variant="primary" name="body-format" id="XML-radio" />
+            <Radio :checked="activeBodyType == 'XML'" @input="setBodyType('XML')" variant="primary" name="body-format"
+                id="XML-radio" />
             XML
         </Label>
+        <textarea @input="updateBodyContent" v-if="activeBodyType == 'JSON'" rows="10"
+            class="block w-full overflow-visible mt-4 bg-background dark:bg-foreground border border-default-200 dark:border-default-700 rounded-md outline-none p-2">{{ tabs[activeTab].body?.content && activeBodyType == 'JSON' ? tabs[activeTab].body?.content?.JSON : "" }}</textarea>
+        <textarea @input="updateBodyContent" v-if="activeBodyType == 'XML'" rows="10"
+            class="block w-full overflow-visible mt-4 bg-background dark:bg-foreground border border-default-200 dark:border-default-700 rounded-md outline-none p-2">{{ tabs[activeTab].body?.content && activeBodyType == "XML" ? tabs[activeTab].body?.content?.XML : "" }}</textarea>
+        <table class="w-full text-left" v-if="activeBodyType == 'x-www-form-urlencoded'">
+            <thead>
+                <tr class="border-t border-b border-default-200 dark:border-default-700 text-default-600 dark:text-default">
+                    <th class="w-8 border border-default-200 dark:border-default-700 p-2 font-normal"></th>
+                    <th class="border border-default-200 dark:border-default-700 p-2 font-normal">Key</th>
+                    <th class="border border-default-200 dark:border-default-700 p-2 font-normal">Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(data, index) in tabs[activeTab].body.content.xWWWFormData"
+                    class="border-t border-b border-default-200 dark:border-default-700 group relative" :key="index">
+                    <td class="border border-default-200 dark:border-default-700 p-2">
+                        <input type="checkbox" class="accent-primary" v-if="data.active != null" v-model="data.active">
+                    </td>
+                    <td class="border border-default-200 dark:border-default-700 p-2"><input type="text"
+                            class="bg-background dark:bg-foreground w-full outline-none focus:bg-default-100 dark:focus:bg-default-800 border border-transparent focus:border-default-200 dark:focus:border-default-700"
+                            v-model="data.key" placeholder="Key" @input="addRowIfNeeded(index)">
+                    </td>
+                    <td class="border border-default-200 dark:border-default-700 p-2"><input type="text"
+                            class="bg-background dark:bg-foreground w-full outline-none focus:bg-default-100 dark:focus:bg-default-800 border border-transparent focus:border-default-200 dark:focus:border-default-700"
+                            v-model="data.value" placeholder="Value" @input="addRowIfNeeded(index)">
+                    </td>
+                    <button title="Eintrag löschen" v-if="data.active != null" @click="rmRow(index)"
+                        class="opacity-0 transition-all rounded absolute mt-1.5 right-1 p-1 group-hover:opacity-100 bg-background dark:bg-foreground hover:bg-default-200 dark:hover:bg-default-700">
+                        <TrashIcon class="text-foreground dark:text-default size-5" />
+                    </button>
+                </tr>
+            </tbody>
+        </table>
+        <table class="w-full text-left" v-if="activeBodyType == 'form-data'">
+            <thead>
+                <tr class="border-t border-b border-default-200 dark:border-default-700 text-default-600 dark:text-default">
+                    <th class="w-8 border border-default-200 dark:border-default-700 p-2 font-normal"></th>
+                    <th class="border border-default-200 dark:border-default-700 p-2 font-normal">Key</th>
+                    <th class="border border-default-200 dark:border-default-700 p-2 font-normal">Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(data, index) in tabs[activeTab].body.content.formData"
+                    class="border-t border-b border-default-200 dark:border-default-700 relative group" :key="index">
+                    <td class="border border-default-200 dark:border-default-700 p-2">
+                        <input type="checkbox" class="accent-primary" v-if="data.active != null" v-model="data.active">
+                    </td>
+                    <td class="border border-default-200 dark:border-default-700 p-2"><input type="text"
+                            class="bg-background dark:bg-foreground w-full outline-none focus:bg-default-100 dark:focus:bg-default-800 border border-transparent focus:border-default-200 dark:focus:border-default-700"
+                            v-model="data.key" placeholder="Key" @input="addRowIfNeeded(index)">
+                    </td>
+                    <td class="border border-default-200 dark:border-default-700 p-2"><input type="text"
+                            class="bg-background dark:bg-foreground w-full outline-none focus:bg-default-100 dark:focus:bg-default-800 border border-transparent focus:border-default-200 dark:focus:border-default-700"
+                            v-model="data.value" placeholder="Value" @input="addRowIfNeeded(index)">
+                    </td>
+                    <button title="Eintrag löschen" v-if="data.active != null" @click="rmRow(index)"
+                        class="opacity-0 transition-all rounded absolute mt-1.5 right-1 p-1 group-hover:opacity-100 bg-background dark:bg-foreground hover:bg-default-200 dark:hover:bg-default-700">
+                        <TrashIcon class="text-foreground dark:text-default size-5" />
+                    </button>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
