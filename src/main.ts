99515ea87a2c54b41c1b2aa2ebaaa62962d00056
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-09-20 14:01:34 +0200

    change settings of toast

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import Toast, { PluginOptions } from "vue-toastification"
+import Toast, { PluginOptions, POSITION } from "vue-toastification"
 import "vue-toastification/dist/index.css"
 
 const app = createApp(App);
@@ -10,6 +10,18 @@ const options: PluginOptions = {
     transition: "Vue-Toastification__bounce",
     maxToasts: 2,
     newestOnTop: true,
+    position: POSITION.BOTTOM_RIGHT,
+    timeout: 6000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.3,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false,
     filterBeforeCreate: (toast, toasts) => {
         if (toasts.filter(
             t => t.type === toast.type
