5eb56cccbed79bc0038bc3078143752f09746adf
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-20 13:52:46 +0200

    install and setup vue-toastification
    setup vue-toastification package for toast messages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import Toast, { PluginOptions } from "vue-toastification"
+import "vue-toastification/dist/index.css"
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const options: PluginOptions = {
+    transition: "Vue-Toastification__bounce",
+    maxToasts: 2,
+    newestOnTop: true,
+    filterBeforeCreate: (toast, toasts) => {
+        if (toasts.filter(
+            t => t.type === toast.type
+        ).length !== 0) {
+            return false;
+        }
+        return toast;
+    }
+}
+
+app.use(Toast, options);
+
+app.mount('#app');
