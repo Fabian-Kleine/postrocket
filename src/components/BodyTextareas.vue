f97e630b2bcd4d7d6ea2dcaae95f096a364dd315
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-23 14:44:39 +0200

    wrap Textarea in div
    warp Textarea in div which adds padding and a border

--- src/components/BodyTextareas.vue
+++ src/components/BodyTextareas.vue
@@ -11,6 +11,8 @@ const { activeBodyType, content } = defineProps<Props>();
 </script>
 
 <template>
-<Textarea v-if="activeBodyType == 'XML'" :language="'xml'"  v-model="content.XML"></Textarea>
-<Textarea v-if="activeBodyType == 'JSON'" :language="'json'" v-model="content.JSON"></Textarea>
+    <div class="px-4 py-2 border border-default-200 dark:border-default-700 rounded-md">
+        <Textarea v-if="activeBodyType == 'XML'" :language="'xml'" v-model="content.XML"></Textarea>
+        <Textarea v-if="activeBodyType == 'JSON'" :language="'json'" v-model="content.JSON"></Textarea>
+    </div>
 </template>
