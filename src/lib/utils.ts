000c08326edee997fb4b9a5fc9b3d7b02317f42a
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-24 08:43:04 +0200

    change colors for methods
    changed some text colors for the methods

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,9 +9,9 @@ export function cn(...inputs) {
 export function methodColors(method: methodsType) {
     if (method.toLowerCase() == "post") return "text-warning";
     if (method.toLowerCase() == "get") return "text-success";
-    if (method.toLowerCase() == "put") return "text-primary";
-    if (method.toLowerCase() == "patch") return "text-violet-500";
-    if (method.toLowerCase() == "delete") return "text-danger";
-    if (method.toLowerCase() == "head") return "text-success";
-    if (method.toLowerCase() == "options") return "text-pink-600";
+    if (method.toLowerCase() == "put") return "text-primary-300";
+    if (method.toLowerCase() == "patch") return "text-violet-400";
+    if (method.toLowerCase() == "delete") return "text-danger-400";
+    if (method.toLowerCase() == "head") return "text-success-400";
+    if (method.toLowerCase() == "options") return "text-pink-500";
 }
