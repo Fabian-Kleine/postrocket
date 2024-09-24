2ee95006bc7a33f0e3f1fd6c285cf228304f5c38
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-24 09:01:55 +0200

    add indicator for active method
    background of selected method is highlighted

--- src/components/MainInput.vue
+++ src/components/MainInput.vue
@@ -40,25 +40,25 @@ function handleMethodInput(e: Event) {
         <div v-if="selectOpen" @click.stop ref="selectRef"
             :class="cn('absolute top-12 left-0 min-w-32 p-2 bg-default-100 dark:bg-default-800 rounded-md shadow-lg z-30')">
             <div @click="tabs[activeTab].method = 'GET'; selectOpen = false;"
-                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('get'))">
+                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('get'), tabs[activeTab].method == 'GET' ? 'bg-default-200 dark:bg-default-700' : '')">
                 GET</div>
             <div @click="tabs[activeTab].method = 'POST'; selectOpen = false;"
-                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('post'))">
+                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('post'), tabs[activeTab].method == 'POST' ? 'bg-default-200 dark:bg-default-700' : '')">
                 POST</div>
             <div @click="tabs[activeTab].method = 'PUT'; selectOpen = false;"
-                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('put'))">
+                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('put'), tabs[activeTab].method == 'PUT' ? 'bg-default-200 dark:bg-default-700' : '')">
                 PUT</div>
             <div @click="tabs[activeTab].method = 'PATCH'; selectOpen = false;"
-                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('patch'))">
+                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('patch'), tabs[activeTab].method == 'PATCH' ? 'bg-default-200 dark:bg-default-700' : '')">
                 PATCH</div>
             <div @click="tabs[activeTab].method = 'DELETE'; selectOpen = false;"
-                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('delete'))">
+                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('delete'), tabs[activeTab].method == 'DELETE' ? 'bg-default-200 dark:bg-default-700' : '')">
                 DELETE</div>
             <div @click="tabs[activeTab].method = 'HEAD'; selectOpen = false;"
-                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('head'))">
+                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('head'), tabs[activeTab].method == 'HEAD' ? 'bg-default-200 dark:bg-default-700' : '')">
                 HEAD</div>
             <div @click="tabs[activeTab].method = 'OPTIONS'; selectOpen = false;"
-                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('options'))">
+                :class="cn('font-bold hover:bg-default-200 dark:hover:bg-default-700 py-1 px-4 cursor-pointer rounded-md', methodColors('options'), tabs[activeTab].method == 'OPTIONS' ? 'bg-default-200 dark:bg-default-700' : '' )">
                 OPTIONS</div>
             <div
                 :class="cn('py-2 mx-2 px-2 mt-3 text-xs text-default-400 dark:text-default border-t border-default-300 dark:border-default')">
