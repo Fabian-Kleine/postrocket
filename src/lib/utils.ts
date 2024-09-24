d6bb98e42093a4893004ecac86b97d0d9b3b9cf5
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-24 15:40:56 +0200

    add buildParams function
    the buildParams function retrun the params from an Array as a urlSearchParams string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { twMerge } from "tailwind-merge";
 import clsx from "clsx";
-import { methodsType } from "../types";
+import { FormDataType, methodsType } from "../types";
 
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
@@ -14,4 +14,18 @@ export function methodColors(method: methodsType) {
     if (method.toLowerCase() == "delete") return "text-danger-400";
     if (method.toLowerCase() == "head") return "text-success-400";
     if (method.toLowerCase() == "options") return "text-pink-500";
+}
+
+export function buildUrlParams(params: Array<FormDataType>): string {
+    try {
+        const url = new URL("http://example.com"); // just a placeholder URL (it has no use)
+
+        params.filter(param => param.active).forEach(param => {
+            url.searchParams.append(param.key, param.value);
+        });
+
+        return url.search;
+    } catch (error) {
+        return "";
+    }
 }
