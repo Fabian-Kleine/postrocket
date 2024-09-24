afeacd24d96ee08f98210c545b500ed01c4bf09f
Fabian Kleine <96775695+Fabian-Kleine@users.noreply.github.com>
2024-09-24 08:34:28 +0200

    add more method colors
    add colors for methods:
    - put
    - patch
    - delete
    - head
    - options

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,4 +9,9 @@ export function cn(...inputs) {
 export function methodColors(method: methodsType) {
     if (method.toLowerCase() == "post") return "text-warning";
     if (method.toLowerCase() == "get") return "text-success";
+    if (method.toLowerCase() == "put") return "text-primary";
+    if (method.toLowerCase() == "patch") return "text-violet-500";
+    if (method.toLowerCase() == "delete") return "text-danger";
+    if (method.toLowerCase() == "head") return "text-success";
+    if (method.toLowerCase() == "options") return "text-pink-600";
 }
