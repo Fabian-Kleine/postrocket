75a3ed847161fc8f66b8226a9029dc8708fa8d66
Fabian Kleine <fabian.kleine@Rhiem.com>
2024-09-26 13:14:01 +0200

    add margin to body textareas

--- src/components/BodyTextareas.vue
+++ src/components/BodyTextareas.vue
@@ -11,7 +11,7 @@ const { activeBodyType, content } = defineProps<Props>();
 </script>
 
 <template>
-    <div v-if="activeBodyType == 'XML' || activeBodyType == 'JSON' || activeBodyType == 'text'"  class="px-4 py-2 border border-default-200 dark:border-default-700 rounded-md">
+    <div v-if="activeBodyType == 'XML' || activeBodyType == 'JSON' || activeBodyType == 'text'"  class="px-4 py-2 mt-2 border border-default-200 dark:border-default-700 rounded-md">
         <Textarea v-if="activeBodyType == 'XML'" :language="'xml'" v-model="content.XML"></Textarea>
         <Textarea v-if="activeBodyType == 'JSON'" :language="'json'" v-model="content.JSON"></Textarea>
         <Textarea v-if="activeBodyType == 'text'" :language="'text'" v-model="content.Text"></Textarea>
